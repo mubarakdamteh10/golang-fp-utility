5fc300d6aeedf656393ea8c3d0518374366d8c89
Mubarak Damteh <might@odds.team>
2024-09-26 16:09:57 +0700

    #15 rename function all -> All again

--- conditional/conditional_test.go
+++ conditional/conditional_test.go
@@ -94,10 +94,10 @@ func TestAll(t *testing.T) {
 		isEven := func(n int) bool {
 			return n%2 == 0
 		}
-		assert.True(t, all(ints, isEven))
+		assert.True(t, All(ints, isEven))
 
 		ints = []int{2, 4, 6, 7}
-		assert.False(t, all(ints, isEven))
+		assert.False(t, All(ints, isEven))
 	})
 
 	t.Run("TestAllWithStrings", func(t *testing.T) {
@@ -107,10 +107,10 @@ func TestAll(t *testing.T) {
 		startsWithA := func(s string) bool {
 			return s[0] == 'a'
 		}
-		assert.True(t, all(strings, startsWithA))
+		assert.True(t, All(strings, startsWithA))
 
 		strings = []string{"apple", "banana", "avocado"}
-		assert.False(t, all(strings, startsWithA))
+		assert.False(t, All(strings, startsWithA))
 	})
 
 	t.Run("TestAllWithStructs", func(t *testing.T) {
@@ -127,14 +127,14 @@ func TestAll(t *testing.T) {
 		valueLessThanFour := func(ts TempStruct) bool {
 			return ts.Value < 4
 		}
-		assert.True(t, all(structs, valueLessThanFour))
+		assert.True(t, All(structs, valueLessThanFour))
 
 		structs = []TempStruct{
 			{Name: "one", Value: 1},
 			{Name: "two", Value: 2},
 			{Name: "four", Value: 4},
 		}
-		assert.False(t, all(structs, valueLessThanFour))
+		assert.False(t, All(structs, valueLessThanFour))
 	})
 
 }
